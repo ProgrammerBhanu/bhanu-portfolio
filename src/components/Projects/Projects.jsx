b46a22ffe0458e6e2449fd8a6a5068afbe686267
Bhanu Pratap Solanki <69785154+ProgrammerBhanu@users.noreply.github.com>
2022-02-22 09:52:33 +0530

    add new project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,13 +3,13 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import { TiHtml5 } from "react-icons/ti";
-import { DiJavascript1, DiNodejs, DiMongodb } from "react-icons/di";
-import { SiCss3, SiBootstrap } from "react-icons/si";
+import { DiJavascript1, DiNodejs, DiMongodb, DiReact } from "react-icons/di";
+import { SiCss3, SiBootstrap, SiRedux } from "react-icons/si";
 
 import mpl from "../../Assets/Projects/mpl.jpg";
 import tatacliq from "../../Assets/Projects/tatacliq.jpg";
 // import editor from "../../Assets/Projects/codeEditor.png";
-import translator from "../../Assets/Projects/translator.jpg";
+import translator from "../../Assets/Projects/translator2.jpg";
 
 function Projects() {
   return (
@@ -56,15 +56,14 @@ function Projects() {
             <ProjectCard
               imgPath={translator}
               isBlog={false}
-              title="Masai - Translator"
-              description="Using HTML, CSS Javascript & API's. A web application that allows users to translate text one language to another.
-              "
-              link1="https://github.com/ProgrammerBhanu/Masai-Translator"
-              link2="https://github.com/ProgrammerBhanu/Masai-Translator"
-              i1={<TiHtml5 />}
-              i2={<DiJavascript1 />}
-              i3={<SiCss3 />}
-              i4={<SiBootstrap />}
+              title="Meesho - Clone"
+              description="Using React, Redux, Node.js, Express.js & MongoDB, I have built with group of 5, a clone of Meesho-clone where users can buy various products."
+              link1="https://github.com/ProgrammerBhanu/meesho-clone"
+              link2="https://meesho-clone.vercel.app/"
+              i1={<DiReact />}
+              i2={<SiRedux />}
+              i3={<DiNodejs />}
+              i4={<DiMongodb />}
             />
           </Col>
         </Row>
